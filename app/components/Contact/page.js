d71260f5e8a373e043f31fa2df5beffbddefc8d7
Mishaaa2024 <mishafatimaali2013@gmail.com>
2024-09-13 11:54:20 +0500

    hopping

--- app/components/Contact/page.js
+++ app/components/Contact/page.js
@@ -10,7 +10,7 @@ const Contact = () => {
     </div>
     <div className="h-full lg:w-1/3 md:w-1/2 bg-gradient-to-r from-purple-500 to-pink-500 flex flex-col md:ml-auto w-full py-6 px-6 mt-8 md:mt-0">
       <h2 className="text-white text-lg mb-1 font-medium title-font">Contact Us</h2>
-      <p className="leading-relaxed mb-5 text-white">Let"s discuss the project.</p>
+      <p className="leading-relaxed mb-5 text-white">Let&quot;s discuss the project.</p>
       <div className="relative mb-4">
         <label for="name" className="leading-7 text-sm text-white">Name</label>
         <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
